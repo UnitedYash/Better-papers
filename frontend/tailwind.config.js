351db2f197f7aaf3118d58ad6ee7e6fdc16c40c4
Ayyash <ayyashahmedanhardeen@gmail.com>
2025-09-02 17:10:09 -0400

    fixed tailwind init

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // looks through all your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
